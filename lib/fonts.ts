b698a94195271d95c4829f413dfc232cf74a7f55
dheereshag <da11@iitbbs.ac.in>
2026-04-08 15:16:12 +0530

    feat: refactor font imports and create fonts utility file

---
+++ lib/fonts.ts
@@ -0,0 +1,19 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+
+export const geistSans = Geist({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  display: "block",
+});
+
+export const geistMono = Geist_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+  display: "block",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "block",
+  variable: "--font-inter",
+});
